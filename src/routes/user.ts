894e92182da3a4640de60bf03e7fd9c5b00fd415
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-08-28 08:57:24 +0800

    feat: expose user endpoints

---
+++ src/routes/user.ts
@@ -0,0 +1,125 @@
+// ANCHOR UUID
+import {
+  v4 as uuidv4,
+  v5 as uuidv5,
+} from 'uuid';
+
+// ANCHOR SERVER
+import SERVER from '../server';
+
+import { CreateUserPayloadInterface, UpdateClientParamsInterface } from '../utils/types';
+import { getUserFromAuthorization } from '../functions/auth';
+import { parseRequestBody } from '../functions/convert';
+import { encrypt } from '../functions/encryption';
+
+SERVER.route({
+  method: 'GET',
+  url: '/user',
+  handler: async (req, res) => {
+    const firebaseUser = await getUserFromAuthorization(req.headers.authorization);
+
+    if (firebaseUser) {
+      const { CLIENT } = await import('../utils/graphql');
+
+      const { user } = await CLIENT.GetUserByPk({
+        id: firebaseUser.uid,
+      });
+
+      if (user) {
+        return res.code(200)
+          .type('application/json; charset=utf-8')
+          .send(user);
+      }
+    }
+
+    return res.code(401)
+      .type('application/json; charset=utf-8')
+      .send({
+        error: 'invalid_owner',
+        error_description: 'Resource owner authentication failed, could be an unknown resource owner, no authentication included, or unsupported authentication method.',
+      });
+  },
+});
+
+SERVER.route({
+  method: 'POST',
+  url: '/user',
+  handler: async (req, res) => {
+    const firebaseUser = await getUserFromAuthorization(req.headers.authorization);
+
+    if (firebaseUser && firebaseUser.email) {
+      const body = parseRequestBody<CreateUserPayloadInterface>(req.body);
+
+      const clientIdLive = uuidv5(String(Date.now()), uuidv4());
+      const clientSecretLive = uuidv5(firebaseUser.uid, clientIdLive);
+      const clientIdTest = uuidv5(String(Date.now()), uuidv4());
+      const clientSecretTest = uuidv5(firebaseUser.uid, clientIdTest);
+
+      const { CLIENT } = await import('../utils/graphql');
+
+      const { user } = await CLIENT.CreateUser({
+        id: firebaseUser.uid,
+        displayName: firebaseUser.displayName ?? 'Anonymous',
+        email: firebaseUser.email,
+        redirectURI: body.redirect_uri,
+        clientIdLive: `pk_live_${encrypt(clientIdLive)}`,
+        clientSecretLive: `sk_live_${encrypt(clientSecretLive)}`,
+        clientIdTest: `pk_test_${encrypt(clientIdTest)}`,
+        clientSecretTest: `sk_test_${encrypt(clientSecretTest)}`,
+      });
+
+      if (user) {
+        return res.code(200)
+          .type('application/json; charset=utf-8')
+          .send(user);
+      }
+    }
+
+    return res.code(401)
+      .type('application/json; charset=utf-8')
+      .send({
+        error: 'invalid_owner',
+        error_description: 'Resource owner authentication failed, could be an unknown resource owner, no authentication included, or unsupported authentication method.',
+      });
+  },
+});
+
+SERVER.route({
+  method: 'PUT',
+  url: '/user/clients/:client_id',
+  handler: async (req, res) => {
+    const firebaseUser = await getUserFromAuthorization(req.headers.authorization);
+
+    if (firebaseUser) {
+      const body = parseRequestBody<CreateUserPayloadInterface>(req.body);
+      const params = req.params as UpdateClientParamsInterface;
+
+      const { CLIENT } = await import('../utils/graphql');
+
+      const { client } = await CLIENT.UpdateClientByPk({
+        id: params.client_id,
+        redirectURI: body.redirect_uri,
+      });
+
+      if (client) {
+        return res.code(200)
+          .type('application/json; charset=utf-8')
+          .send(client);
+      }
+
+      return res.code(400)
+        .type('application/json; charset=utf-8')
+        .send({
+          error: 'invalid_request',
+          error_description: 'Unknown client',
+        });
+    }
+
+    return res.code(401)
+      .type('application/json; charset=utf-8')
+      .send({
+        error: 'invalid_owner',
+        error_description: 'Resource owner authentication failed, could be an unknown resource owner, no authentication included, or unsupported authentication method.',
+      });
+  },
+});
--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -2,3 +2,11 @@ export interface VerifyTokenQueryInterface {
   token: string;
   token_type_hint: 'access_token' | 'refresh_token';
 }
+
+export interface CreateUserPayloadInterface {
+  redirect_uri?: string;
+}
+
+export interface UpdateClientParamsInterface {
+  client_id: string;
+}
